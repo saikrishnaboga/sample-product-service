3cc755fe079d29656622d06697ab646499a719e9
saikrishnaboga <saikrishnaboga29@gmail.com>
2025-07-26 18:51:23 +0530

    client created for Fakestore

---
+++ src/main/java/dev/krishna/productservice/thirdpartyclients/fakestore/FakeStoreProductCilent.java
@@ -0,0 +1,77 @@
+package dev.krishna.productservice.thirdpartyclients.fakestore;
+
+import dev.krishna.productservice.dtos.FakeStoreProductDto;
+import dev.krishna.productservice.dtos.GenericProductDto;
+import dev.krishna.productservice.exceptions.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FakeStoreProductCilent {
+
+    private String productUrl = "https://fakestoreapi.com/products/{id}";
+    private String productRequestUrl = "https://fakestoreapi.com/products/";
+
+    private RestTemplateBuilder restTemplateBuilder;
+
+    @Autowired
+    public FakeStoreProductCilent(RestTemplateBuilder restTemplateBuilder){
+        this.restTemplateBuilder = restTemplateBuilder;
+    }
+
+
+
+
+    public FakeStoreProductDto getProductById(Long id) throws NotFoundException {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        ResponseEntity<FakeStoreProductDto> response = restTemplate.getForEntity(productUrl, FakeStoreProductDto.class, id);
+
+        FakeStoreProductDto fakeStoreProductDto = response.getBody();
+
+        if(fakeStoreProductDto == null){
+            throw new NotFoundException("Product with id: " + id + " not found");
+        }
+
+        return  fakeStoreProductDto;
+    }
+
+
+
+
+    public FakeStoreProductDto createProduct(FakeStoreProductDto product) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        ResponseEntity<FakeStoreProductDto> response =  restTemplate.postForEntity(
+                productRequestUrl, product, FakeStoreProductDto.class);
+
+        FakeStoreProductDto fakeStoreProductDto = response.getBody();
+
+        return fakeStoreProductDto;
+    }
+
+    public List<FakeStoreProductDto> getAllProducts() {
+
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        ResponseEntity<FakeStoreProductDto[]> response =
+                restTemplate.getForEntity(productRequestUrl, FakeStoreProductDto[].class);
+
+//        ParameterizedTypeReference<List<FakeStoreProductDto>> other way to achieve  it
+        FakeStoreProductDto[] fakeStoreProductDtos = response.getBody();
+
+        return Arrays.asList(fakeStoreProductDtos);
+
+    }
+
+    public FakeStoreProductDto deleteProduct(Long id) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        ResponseEntity<FakeStoreProductDto> response =   restTemplate.exchange(productUrl, HttpMethod.DELETE, null, FakeStoreProductDto.class, id);
+
+        FakeStoreProductDto fakeStoreProductDto = response.getBody();
+        return fakeStoreProductDto;
+    }
+}
