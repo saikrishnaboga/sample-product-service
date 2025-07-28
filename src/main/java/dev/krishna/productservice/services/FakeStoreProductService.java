f7e1b7098ae2072fb79bd609607c6443042b2756
saikrishnaboga <saikrishnaboga29@gmail.com>
2025-07-28 22:00:44 +0530

    optimize used primary

--- src/main/java/dev/krishna/productservice/services/FakeStoreProductService.java
+++ src/main/java/dev/krishna/productservice/services/FakeStoreProductService.java
@@ -8,6 +8,7 @@ import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.ParameterResolutionDelegate;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
 
-
+@Primary
 @Service("fakeStoreProductService")
 public class FakeStoreProductService implements ProductService{
 
