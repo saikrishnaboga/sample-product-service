a7c9272fe64ff290a8ac9348488a2da59cce50cc
saikrishnaboga <saikrishnaboga29@gmail.com>
2025-08-11 20:57:43 +0530

    uuid created

--- src/main/java/dev/krishna/productservice/models/Product.java
+++ src/main/java/dev/krishna/productservice/models/Product.java
@@ -1,7 +1,10 @@
 package dev.krishna.productservice.models;
 
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.UUID;
 
 @Getter
 @Setter
@@ -9,6 +12,13 @@ public class Product extends BaseModel {
     private String title;
     private String description;
     private String image;
+
+    @ManyToOne
     private String category;
     private double price;
+
+    public static void main(String args[]){
+        UUID id = new UUID.randomUUID();
+        System.out.println(id);
+    }
 }
