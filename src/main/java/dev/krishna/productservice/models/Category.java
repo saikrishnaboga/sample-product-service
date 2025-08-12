4f0f578c5a8dba37f3d4314a0460a69e53a35721
saikrishnaboga <saikrishnaboga29@gmail.com>
2025-08-12 20:47:02 +0530

    entity annotations added

--- src/main/java/dev/krishna/productservice/models/Category.java
+++ src/main/java/dev/krishna/productservice/models/Category.java
@@ -1,10 +1,12 @@
 package dev.krishna.productservice.models;
 
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Category extends BaseModel {
-
+    private String name;
 }
--- src/main/java/dev/krishna/productservice/models/Product.java
+++ src/main/java/dev/krishna/productservice/models/Product.java
@@ -1,5 +1,6 @@
 package dev.krishna.productservice.models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel {
     private String title;
     private String description;
@@ -17,8 +19,8 @@ public class Product extends BaseModel {
     private String category;
     private double price;
 
-    public static void main(String[] args){
-        UUID id = UUID.randomUUID();
-        System.out.println(id);
-    }
+//    public static void main(String[] args){
+//        UUID id = UUID.randomUUID();
+//        System.out.println(id);
+//    }
 }
