1187e4843f904d7afd4c84e46849b0ee2734f3fd
saikrishnaboga <saikrishnaboga29@gmail.com>
2025-08-16 12:20:40 +0530

    uuidgenerator added in basemodel

--- src/main/java/dev/krishna/productservice/models/BaseModel.java
+++ src/main/java/dev/krishna/productservice/models/BaseModel.java
@@ -1,17 +1,19 @@
 package dev.krishna.productservice.models;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
 
 @Getter
 @Setter
 @MappedSuperclass
 public class BaseModel {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(generator = "uuidgenerator")
+    @GenericGenerator(name = "uuidgenerator", strategy = "uuid2")
+    @Column(name = "id", columnDefinition = "Binary(16)",  nullable=false, updatable=false )
+    private UUID id;
 }
